9198983b0373287bc78a2bd9f6ef48610f572bb9
Dakota Coppage <dakotakcoppage@me.com>
2020-08-12 04:42:55 +0000

    Automatically backed up by Learn

--- lib/meowing_cat.rb
+++ lib/meowing_cat.rb
@@ -1,27 +1,3 @@
 class Cat 
-  def name=(name)
-    @name = name
-  end
-  
-  def name
-    @name
-  end
-  
-  def meow=(meow)
-    @meow = meow
-  end
-  
-  def meow
-    @meow
-  end
-end
-
-maru = Cat.name
-maru.name = "Maru"
-
-meow = Cat.meow
-maru.meow = "meow!"
-
-class Cat 
   attr_accessor
-end
+  
